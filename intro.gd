36add163d0189e1ead6cbaec0b6614d77a93b3c9
Chaitanya Vaswani <vaswanichaitanya@gmail.com>
2024-11-20 07:22:51 +0530

    menu is also commited

--- intro.gd
+++ intro.gd
@@ -1,4 +1,5 @@
 extends Node2D
+const a =preload("res://scenes/menu.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,4 +10,4 @@ func play_fade_sequence() -> void:
 	await get_tree().create_timer(6).timeout
 	$AnimationPlayer.play("fadeout")
 	await get_tree().create_timer(3).timeout
-	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+	get_tree().change_scene_to_packed(a)
